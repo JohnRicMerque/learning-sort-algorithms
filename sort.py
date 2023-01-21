d14ddf9a8f65df46fa0ab4cfb4e91251770f2ee8
John Ric Merque <johnricmer@gmail.com>
2023-01-21 17:42:34 +0800

    declared selection sort function and looped according to index length

--- sort.py
+++ sort.py
@@ -1,2 +1,8 @@
 arrayValues = [49, 7, 31, 29, 58, 20, 95, 83, 60, 81]
 
+def selectionSort(arr):
+    for i in range (9): # loop through indexes
+        minpos = i
+        print(minpos)
+
+print(selectionSort(arrayValues))
