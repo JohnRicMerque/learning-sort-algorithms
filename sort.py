cbdc12f8b55dcd529fe7f9f5eaac09d0352dcd9d
John Ric Merque <johnricmer@gmail.com>
2023-01-21 17:58:02 +0800

    added nested array that loops through the rest of the array values and finds minimum value

--- sort.py
+++ sort.py
@@ -1,8 +1,13 @@
 arrayValues = [49, 7, 31, 29, 58, 20, 95, 83, 60, 81]
 
 def selectionSort(arr):
-    for i in range (9): # loop through indexes
-        minpos = i
-        print(minpos)
+    for i in range (9): # loop through array indexes
+        minpos = i # initially declared minimum position as the i
+        for j in range(i, 10): # nested loop starting from i index to the rest
+            if arr[j] < arr[minpos]: # if value at index j is less than value at index min position
+                minpos = j # reassigns value of minimum position to j
+    
+    print(minpos)
 
-print(selectionSort(arrayValues))
+
+selectionSort(arrayValues)
