7da281f9206ddf1d0f28e4117739982429ea3d15
John Ric Merque <johnricmer@gmail.com>
2023-01-21 18:28:45 +0800

    edited notes for clarity

--- sort.py
+++ sort.py
@@ -1,13 +1,18 @@
 def selectionSort(arr):
-    for i in range (9): # loop through array indexes
-        minpos = i # initially declared minimum position as the i
-        for j in range(i, 10): # nested loop starting from i index to the rest
-            if arr[j] < arr[minpos]: # if value at index j is less than value at index min position
-                minpos = j # reassigns value of minimum position to j
+    for i in range (9): # loop through array indexes [0-9] and minimum position will be the i index every loop
+                        # starts from 0 after one iteration it will be filled, then index 1 will be the minimum
+                        # this makes it sequential in the array
+        minpos = i 
+        for j in range(i, 10): # to search for the minimum value then assign its index to minposition variable
+            if arr[j] < arr[minpos]: 
+                minpos = j 
 
-        # this code below switches the array value from the one we declared min position, with the one we found to be minimum value. the loop will search for the minimum value to the right, not including the values in the previous iterations, which were the minimum values found prior.
+        # this code below switches the array value from the one initially in the index we declared min position, 
+        # with the one we found to be minimum value. 
+        # the loop will search for the minimum value to the right, not including the values in
+        # the previous iterations, which were the minimum values found prior.
         temp = arr[i] 
-        arr[i] = arr[minpos] # replace the new minimim value in index of initially considered minimum value
+        arr[i] = arr[minpos] # assign the minimim value in min position
         arr[minpos] = temp # put the replaced value in the initial index of minimum value
 
 arrayValues = [49, 7, 31, 29, 58, 20, 95, 83, 60, 81]
